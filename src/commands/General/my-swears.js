3f7fb0ee3985e9cb364d9b27805e01b152c98b7c
Hutch <tech_hutch@verizon.net>
2018-10-30 10:57:51 -0400

    added myswears cmd

---
+++ src/commands/General/my-swears.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['myswears'],
+			description: '🗣 👀',
+			extendedHelp: [
+				'I see you swearing 👀',
+				'',
+				"Don't worry, tho, I like swearing. I just have too much self control to do it myself 😛",
+			],
+		});
+	}
+
+	run(msg) {
+		const { profanity } = msg.author.settings;
+		const embed = new MessageEmbed();
+		for (const [word, counter] of Object.entries(profanity)) {
+			embed.addField(`${word[0]}-word`, counter, true);
+		}
+		return msg.sendEmbed(embed);
+	}
+
+};
