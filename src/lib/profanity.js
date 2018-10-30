6374390c897503299ff121208771084f97ab44c8
Hutch <tech_hutch@verizon.net>
2018-10-29 20:34:02 -0400

    new words

--- src/lib/profanity.js
+++ src/lib/profanity.js
@@ -5,36 +5,48 @@ module.exports = { regexes: profanityRegexes };
 ({ exports } = module);
 
 const sEnds = ['s', 'ing', 'ed'];
+const sErEnds = [...sEnds, 'ers?'];
 const esEnds = ['es', 'ing', 'ed'];
+const esErEnds = [...esEnds, 'ers?'];
 /**
- * @type {([string, string[], (string[] | undefined)])[]}
+ * @type {([string, string[]?, string[]?])[]}
  */
 const profanityToAssemble = [
 	// Excrement
-	['shit', ['s', 't?ing', 't?ed'], ['shat']],
-	['piss', esEnds],
-	['crap', ['s', 'p?ing', 'p?ed']],
+	['shit', ['s', 't?ers?', 't?ing', 't?ed'], ['shat', 'shite']],
+	['piss', esErEnds],
+	['crap', ['s', 'p?ers?', 'p?ing', 'p?ed']],
 	// Body parts
-	['dick', sEnds],
+	['cunt', sEnds],
 	['cock', sEnds],
+	['dick', sEnds],
 	['pussy', sEnds, ['pussies', 'pussied']],
-	['ass', esEnds],
+	['bollock', ['s']],
 	['asshole', ['s']],
+	['arsehole', ['s']],
+	['ass', esEnds],
+	['arse', esEnds],
 	// Insults
 	['bitch', esEnds],
 	['fag', ['s', 'g?ing', 'ged'], ['faggot', 'faggots']],
 	['bastard', ['s']],
-	['douche', ['s'], ['douching', 'douched']],
 	['slut', ['s']],
+	['douche', ['s'], ['douching', 'douched']],
+	// Sexual
+	['fuck', sErEnds],
+	['bugger', sEnds],
+	['wank', sErEnds],
 	// General & religious
-	['fuck', sEnds],
+	['goddamn', ['ed'], ['gdi']],
+	['hell'],
 	['bloody'],
 	['damn', sEnds],
 	['darn', ['ed']],
 ];
 for (const array of profanityToAssemble) {
 	assert(typeof array[0] === 'string' && array[0].length);
-	assert(Array.isArray(array[1]) && array[1].length);
+	// eslint-disable-next-line eqeqeq
+	assert((Array.isArray(array[1]) && array[1].length) || array[1] == null);
 	assert(typeof array[2] === 'undefined' || Array.isArray(array[2]));
 	assert([2, 3].includes(array.length));
 }
