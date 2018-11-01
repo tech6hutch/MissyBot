363a21550f62790b93ef683d47f0b41c7cc1d4c8
Hutch <tech_hutch@verizon.net>
2018-11-01 14:24:08 -0400

    I'm already requiring this in MissyClient.js, I don't need to preload it

--- src/lib/preload.js
+++ src/lib/preload.js
@@ -1,5 +1,2 @@
 // Go ahead and load these files so that they're already cached
 require('canvas-constructor');
-
-// My files; errors will crash the bot *before* it logs in
-require('./profanity');
