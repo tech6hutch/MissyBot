efbe1cea63f26ee10df0f6d3b414b7176c55a754
Hutch <tech_hutch@verizon.net>
2018-11-01 09:30:12 -0400

    missyOnly inhibitor: added moonbeam to allowed users

--- src/inhibitors/missyOnly.js
+++ src/inhibitors/missyOnly.js
@@ -9,7 +9,12 @@ module.exports = class extends Inhibitor {
 	}
 
 	init() {
-		this.allowedIDs.push(this.client.owner.id, this.client.missyID);
+		this.allowedIDs.push(
+			this.client.owner.id,
+			this.client.missyID,
+		);
+		const moonbeamID = '214442156788678658';
+		if (this.client.users.has(moonbeamID)) this.allowedIDs.push(moonbeamID);
 	}
 
 	async run(msg, cmd) {
