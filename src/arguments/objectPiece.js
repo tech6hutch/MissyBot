2a5ef6720f92ff250255880b7723651dee1a97df
Hutch <tech_hutch@verizon.net>
2018-11-01 14:28:06 -0400

    disable unused argument

--- src/arguments/objectPiece.js
+++ src/arguments/objectPiece.js
@@ -2,6 +2,10 @@ const { Argument } = require('klasa');
 
 module.exports = class extends Argument {
 
+	constructor(...args) {
+		super(...args, { enabled: false });
+	}
+
 	run(arg, possible, message) {
 		const object = this.client.objects.get(arg);
 		if (object) return object;
