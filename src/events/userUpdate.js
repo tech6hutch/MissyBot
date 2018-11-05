61f8976790d231e653b837241c4776946674c16e
Hutch <tech_hutch@verizon.net>
2018-11-05 08:27:16 -0500

    added an event to copy Missy's avatar whenever she changes it

---
+++ src/events/userUpdate.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { Event } = require('klasa');
+
+module.exports = class extends Event {
+
+	async run(oldUser, newUser) {
+		assert(oldUser.id === newUser.id);
+		if (newUser.id !== this.client.missyID || oldUser.avatar === newUser.avatar) return;
+
+		this.copyUserAvatar(newUser);
+	}
+
+	copyUserAvatar(user) {
+		return this.client.user.setAvatar(user.displayAvatarURL({ size: 2048 }));
+	}
+
+};
