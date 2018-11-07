5462fc0c8663b8eb3e89ca2385ec71f2c260c4d3
Hutch <tech_hutch@verizon.net>
2018-11-06 23:45:12 -0500

    type cmd bugfixes
    - fixed infinite typing (the docs say it stops "gracefully", but that
      never seems to come)
    - fixed not getting the full name from usage

--- src/commands/Fun/type.js
+++ src/commands/Fun/type.js
@@ -1,11 +1,12 @@
 const { Command } = require('klasa');
+const { naturalPause } = require('../../lib/util/util');
 
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
 			description: lang => lang.get('COMMAND_TYPE_DESCRIPTION'),
-			usage: '<as> <name:str>',
+			usage: '<as> <name:...str>',
 			usageDelim: ' ',
 			extendedHelp: lang => lang.get('COMMAND_TYPE_EXTENDEDHELP',
 				'Missy, type as FBI'),
@@ -16,7 +17,9 @@ module.exports = class extends Command {
 		const { me } = msg.guild;
 		const { nickname } = me;
 		await me.setNickname(name, 'type cmd start');
-		await msg.channel.startTyping();
+		msg.channel.startTyping();
+		await naturalPause('long');
+		msg.channel.stopTyping();
 		await me.setNickname(nickname, 'type cmd end');
 	}
 
