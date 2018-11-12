7f52e24f0d237cac69f1ebb7822f56aef3580f0a
Hutch <tech_hutch@verizon.net>
2018-11-12 10:48:24 -0500

    emoji cmd: changed to accept multiple names at once, added --image flag

--- src/commands/General/emoji.js
+++ src/commands/General/emoji.js
@@ -5,14 +5,17 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
 			description: '<:discord:503738729463021568>😮',
-			usage: '<name:str>',
+			usage: '<name:str> [...]',
+			usageDelim: ' ',
 			aliases: ['emote'],
 		});
 	}
 
-	async run(msg, [name]) {
-		const emoji = this.client.emojis.find(e => e.name === name);
-		return msg.send(emoji ? emoji.toString() : '❓');
+	async run(msg, names) {
+		const emojis = this.client.emojis.array().filter(e => names.includes(e.name));
+		return msg.send(emojis.length ?
+			('image' in msg.flags ? emojis.map(e => e.url) : emojis).join(' ') :
+			'❓');
 	}
 
 };
