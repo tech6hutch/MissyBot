85b33932b85e032c25f38d65f0d504563ce56f9a
Hutch <tech_hutch@verizon.net>
2018-12-01 11:40:13 -0500

    added Rye reaction

---
+++ src/monitors/specialReact.js
@@ -0,0 +1,22 @@
+const { Monitor } = require('klasa');
+
+module.exports = class extends Monitor {
+
+	constructor(...args) {
+		super(...args, {
+			ignoreOthers: false,
+		});
+		// Yes I'm intentionally leaving off a second word boundary
+		this.regex = /\bpoop/i;
+	}
+
+	async run(msg) {
+		if (msg.author === this.client.missy) {
+			if (this.regex.test(msg.content)) return msg.react('💩');
+		} else if (msg.author.id === '156757401201016832') {
+			return msg.react('🍞');
+		}
+		return null;
+	}
+
+};
