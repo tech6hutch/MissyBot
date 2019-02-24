a7fb19961ac3a09fbd6d849c365816b58a99a397
Hutch <tech_hutch@verizon.net>
2019-02-24 16:26:15 -0500

    added special reactions for Lexi

--- src/monitors/specialReacts.ts
+++ src/monitors/specialReacts.ts
@@ -7,13 +7,20 @@ export default class extends MissyMonitor {
 	// Only has a word boundary at the beginning so it matches e.g. "pooping"
 	missyPoopRegex = /\bpoop/i;
 
+	// lexID = '280175211737776128';
+	lexID = '224236171838881792'; // mine, for testing
+
 	constructor(client: MissyClient, store: MonitorStore, file: string[], directory: string) {
 		super(client, store, file, directory, { ignoreOthers: false });
 	}
 
-	run(msg: KlasaMessage) {
+	async run(msg: KlasaMessage) {
 		if (msg.author === this.client.missy) {
 			if (this.missyPoopRegex.test(msg.content)) return msg.react('💩');
+		} else if (msg.author.id === this.lexID) {
+			const reactionPromise = msg.react('👰🏽');
+			if (msg.content.toLowerCase().startsWith('wife')) return [await reactionPromise, await msg.send('Lexi!')];
+			return reactionPromise;
 		}
 		return null;
 	}
