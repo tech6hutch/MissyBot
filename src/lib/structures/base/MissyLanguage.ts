6ee10ce98f29f2b76a5b5ca89dccdbf8fbbf7dc4
Hutch <tech_hutch@verizon.net>
2019-07-31 16:41:47 -0400

    override get method to handle RandomResponses

--- src/lib/structures/base/MissyLanguage.ts
+++ src/lib/structures/base/MissyLanguage.ts
@@ -1,11 +1,17 @@
-import {
-	Language,
-} from 'klasa';
+import { Language } from 'klasa';
 import MissyClient from '../../MissyClient';
+import RandomResponse from '../../util/RandomResponse';
 
 export default abstract class MissyLanguage extends Language {
 
 	// @ts-ignore assigned in the parent class
 	readonly client: MissyClient;
+
+	get<T = string>(term: string, ...args: any[]): T {
+		const value: T | RandomResponse = super.get(term, ...args);
+		return typeof value === 'object' && value instanceof RandomResponse ?
+			value.run(this.client, args[0], ...args) as any as T :
+			value;
+	}
 
 }
